8a1edd4dd9276ba7ca2a95a4a06d0f3ad73b6907
Hizuro <hizuro@gmx.net>
2018-07-19 14:25:04 +0200

    update races table

--- LibRaces-1.0.lua
+++ LibRaces-1.0.lua
@@ -25,162 +25,31 @@ local language = {};
 
 -- data[<engLowerStripped>] = { <langCode> = { <langCode> = { <male>, <female> } } }
 local data = {
-	["human"] = {
-		deDE = {"Mensch","Mensch"},
-		enUS = {"Human","Human"},
-		esES = {"Humano","Humana"},
-		frFR = {"Humain","Humaine"},
-		itIT = {"Umano","Umana"},
-		koKR = {"인간","인간"},
-		ptBR = {"Humano","Humana"},
-		ruRU = {"Человек","Человек"},
-		zhCN = {"人类","人类"},
-		zhTW = {"人類","人類"}
-	},
-	["orc"] = {
-		deDE = {"Orc","Orcs"},
-		enUS = {"Orc","Orc"},
-		esES = {"Orco","Orco"},
-		frFR = {"Orc","Orque"},
-		itIT = {"Orco","Orchessa"},
-		koKR = {"오크","오크"},
-		ptBR = {"Orc","Orquisa"},
-		ruRU = {"Орк","Орк"},
-		zhCN = {"兽人","兽人"},
-		zhTW = {"獸人","獸人"}
-	},
-	["dwarf"] = {
-		deDE = {"Zwerg","Zwergin"},
-		enUS = {"Dwarf","Dwarf"},
-		esES = {"Enano","Enana"},
-		frFR = {"Nain","Naine"},
-		itIT = {"Nano","Nana"},
-		koKR = {"드워프","드워프"},
-		ptBR = {"Anão","Anã"},
-		ruRU = {"Дворф","Дворф"},
-		zhCN = {"矮人","矮人"},
-		zhTW = {"矮人","矮人"}
-	},
-	["nightelf"] = {
-		deDE = {"Nachtelf","Nachtelfe"},
-		enUS = {"Night Elf","Night Elf"},
-		esES = {"Elfo de la noche","Elfa de la noche"},
-		frFR = {"Elfe de la nuit","Elfe de la nuit"},
-		itIT = {"Elfo della Notte","Elfa della Notte"},
-		koKR = {"나이트 엘프","나이트 엘프"},
-		ptBR = {"Elfo Noturno","Elfa Noturna"},
-		ruRU = {"Ночной эльф","Ночная эльфийка"},
-		zhCN = {"暗夜精灵","暗夜精灵"},
-		zhTW = {"夜精靈","夜精靈"}
-	},
-	["undead"] = {
-		deDE = {"Untoter","Untote"},
-		enUS = {"Undead","Undead"},
-		esES = {"No-muerto","No-muerta"},
-		frFR = {"Mort-vivant","Morte-vivante"},
-		itIT = {"Non Morto","Non Morta"},
-		koKR = {"언데드","언데드"},
-		ptBR = {"Morto-vivo","Morta-viva"},
-		ruRU = {"Нежить","Нежить"},
-		zhCN = {"亡灵","亡灵"},
-		zhTW = {"不死族","不死族"}
-	},
-	["tauren"] = {
-		deDE = {"Tauren","Tauren"},
-		enUS = {"Tauren","Tauren"},
-		esES = {"Tauren","Tauren"},
-		frFR = {"Tauren","Taurène"},
-		itIT = {"Tauren","Tauren"},
-		koKR = {"타우렌","타우렌"},
-		ptBR = {"Tauren","Taurena"},
-		ruRU = {"Таурен","Таурен"},
-		zhCN = {"牛头人","牛头人"},
-		zhTW = {"牛頭人","牛頭人"}
-	},
-	["gnome"] = {
-		deDE = {"Gnom","Gnom"},
-		enUS = {"Gnome","Gnome"},
-		esES = {"Gnomo","Gnoma"},
-		frFR = {"Gnome","Gnome"},
-		itIT = {"Gnomo","Gnoma"},
-		koKR = {"노움","노움"},
-		ptBR = {"Gnomo","Gnomida"},
-		ruRU = {"Гном","Гном"},
-		zhCN = {"侏儒","侏儒"},
-		zhTW = {"地精","地精"}
-	},
-	["troll"] = {
-		deDE = {"Troll","Trolle"},
-		enUS = {"Troll","Troll"},
-		esES = {"Trol","Trol"},
-		frFR = {"Troll","Trollesse"},
-		itIT = {"Troll","Troll"},
-		koKR = {"트롤","트롤"},
-		ptBR = {"Troll","Trolesa"},
-		ruRU = {"Тролль","Тролль"},
-		zhCN = {"巨魔","巨魔"},
-		zhTW = {"食人妖","食人妖"}
-	},
-	["goblin"] = {
-		deDE = {"Goblin","Goblin"},
-		enUS = {"Goblin","Goblin"},
-		esES = {"Goblin","Goblin"},
-		frFR = {"Gobelin","Gobeline"},
-		itIT = {"Goblin","Goblin"},
-		koKR = {"고블린","고블린"},
-		ptBR = {"Goblin","Goblina"},
-		ruRU = {"Гоблин","Гоблин"},
-		zhCN = {"地精","地精"},
-		zhTW = {"哥布林","哥布林"}
-	},
-	["bloodelf"] = {
-		deDE = {"Blutelf","Blutelfe"},
-		enUS = {"Blood Elf","Blood Elf"},
-		esES = {"Elfo de sangre","Elfa de sangre"},
-		frFR = {"Elfe de sang","Elfe de sang"},
-		itIT = {"Elfo del Sangue","Elfa del Sangue"},
-		koKR = {"블러드 엘프","블러드 엘프"},
-		ptBR = {"Elfo Sangrento","Elfa Sangrenta"},
-		ruRU = {"Эльф крови","Эльфийка крови"},
-		zhCN = {"血精灵","血精灵"},
-		zhTW = {"血精靈","血精靈"}
-	},
-	["draenei"] = {
-		deDE = {"Draenei","Draenei"},
-		enUS = {"Draenei","Draenei"},
-		esES = {"Draenei","Draenei"},
-		frFR = {"Draeneï","Draeneï"},
-		itIT = {"Draenei","Draenei"},
-		koKR = {"드레나이","드레나이"},
-		ptBR = {"Draenei","Draenaia"},
-		ruRU = {"Дреней","Дренейка"},
-		zhCN = {"德莱尼","德莱尼"},
-		zhTW = {"德萊尼","德萊尼"}
-	},
-	["worgen"] = {
-		deDE = {"Worgen","Worgen"},
-		enUS = {"Worgen","Worgen"},
-		esES = {"Huargen","Huargen"},
-		frFR = {"Worgen","Worgen"},
-		itIT = {"Worgen","Worgen"},
-		koKR = {"늑대인간","늑대인간"},
-		ptBR = {"Worgen","Worgenin"},
-		ruRU = {"Ворген","Ворген"},
-		zhCN = {"狼人","狼人"},
-		zhTW = {"狼人","狼人"}
-	},
-	["pandaren"] = {
-		deDE = {"Pandaren","Pandaren"},
-		enUS = {"Pandaren","Pandaren"},
-		esES = {"Pandaren","Pandaren"},
-		frFR = {"Pandaren","Pandarène"},
-		itIT = {"Pandaren","Pandaren"},
-		koKR = {"판다렌","판다렌"},
-		ptBR = {"Pandaren","Pandarena"},
-		ruRU = {"Пандарен","Пандарен"},
-		zhCN = {"熊猫人","熊猫人"},
-		zhTW = {"熊貓人","熊貓人"}
-	}
+	["human"] = { deDE = {"Mensch","Mensch"}, enUS = {"Human","Human"}, esES = {"Humano","Humana"}, frFR = {"Humain","Humaine"}, itIT = {"Umano","Umana"}, koKR = {"인간","인간"}, ptBR = {"Humano","Humana"}, ruRU = {"Человек","Человек"}, zhCN = {"人类","人类"}, zhTW = {"人類","人類"} },
+	["orc"] = { deDE = {"Orc","Orcs"}, enUS = {"Orc","Orc"}, esES = {"Orco","Orco"}, frFR = {"Orc","Orque"}, itIT = {"Orco","Orchessa"}, koKR = {"오크","오크"}, ptBR = {"Orc","Orquisa"}, ruRU = {"Орк","Орк"}, zhCN = {"兽人","兽人"}, zhTW = {"獸人","獸人"} },
+	["dwarf"] = { deDE = {"Zwerg","Zwergin"}, enUS = {"Dwarf","Dwarf"}, esES = {"Enano","Enana"}, frFR = {"Nain","Naine"}, itIT = {"Nano","Nana"}, koKR = {"드워프","드워프"}, ptBR = {"Anão","Anã"}, ruRU = {"Дворф","Дворф"}, zhCN = {"矮人","矮人"}, zhTW = {"矮人","矮人"} },
+	["nightelf"] = { deDE = {"Nachtelf","Nachtelfe"}, enUS = {"Night Elf","Night Elf"}, esES = {"Elfo de la noche","Elfa de la noche"}, frFR = {"Elfe de la nuit","Elfe de la nuit"}, itIT = {"Elfo della Notte","Elfa della Notte"}, koKR = {"나이트 엘프","나이트 엘프"}, ptBR = {"Elfo Noturno","Elfa Noturna"}, ruRU = {"Ночной эльф","Ночная эльфийка"}, zhCN = {"暗夜精灵","暗夜精灵"}, zhTW = {"夜精靈","夜精靈"} },
+	["undead"] = { deDE = {"Untoter","Untote"}, enUS = {"Undead","Undead"}, esES = {"No-muerto","No-muerta"}, frFR = {"Mort-vivant","Morte-vivante"}, itIT = {"Non Morto","Non Morta"}, koKR = {"언데드","언데드"}, ptBR = {"Morto-vivo","Morta-viva"}, ruRU = {"Нежить","Нежить"}, zhCN = {"亡灵","亡灵"}, zhTW = {"不死族","不死族"} },
+	["tauren"] = { deDE = {"Tauren","Tauren"}, enUS = {"Tauren","Tauren"}, esES = {"Tauren","Tauren"}, frFR = {"Tauren","Taurène"}, itIT = {"Tauren","Tauren"}, koKR = {"타우렌","타우렌"}, ptBR = {"Tauren","Taurena"}, ruRU = {"Таурен","Таурен"}, zhCN = {"牛头人","牛头人"}, zhTW = {"牛頭人","牛頭人"} },
+	["gnome"] = { deDE = {"Gnom","Gnom"}, enUS = {"Gnome","Gnome"}, esES = {"Gnomo","Gnoma"}, frFR = {"Gnome","Gnome"}, itIT = {"Gnomo","Gnoma"}, koKR = {"노움","노움"}, ptBR = {"Gnomo","Gnomida"}, ruRU = {"Гном","Гном"}, zhCN = {"侏儒","侏儒"}, zhTW = {"地精","地精"} },
+	["troll"] = { deDE = {"Troll","Trolle"}, enUS = {"Troll","Troll"}, esES = {"Trol","Trol"}, frFR = {"Troll","Trollesse"}, itIT = {"Troll","Troll"}, koKR = {"트롤","트롤"}, ptBR = {"Troll","Trolesa"}, ruRU = {"Тролль","Тролль"}, zhCN = {"巨魔","巨魔"}, zhTW = {"食人妖","食人妖"} },
+	["goblin"] = { deDE = {"Goblin","Goblin"}, enUS = {"Goblin","Goblin"}, esES = {"Goblin","Goblin"}, frFR = {"Gobelin","Gobeline"}, itIT = {"Goblin","Goblin"}, koKR = {"고블린","고블린"}, ptBR = {"Goblin","Goblina"}, ruRU = {"Гоблин","Гоблин"}, zhCN = {"地精","地精"}, zhTW = {"哥布林","哥布林"} },
+	["bloodelf"] = { deDE = {"Blutelf","Blutelfe"}, enUS = {"Blood Elf","Blood Elf"}, esES = {"Elfo de sangre","Elfa de sangre"}, frFR = {"Elfe de sang","Elfe de sang"}, itIT = {"Elfo del Sangue","Elfa del Sangue"}, koKR = {"블러드 엘프","블러드 엘프"}, ptBR = {"Elfo Sangrento","Elfa Sangrenta"}, ruRU = {"Эльф крови","Эльфийка крови"}, zhCN = {"血精灵","血精灵"}, zhTW = {"血精靈","血精靈"} },
+	["draenei"] = { deDE = {"Draenei","Draenei"}, enUS = {"Draenei","Draenei"}, esES = {"Draenei","Draenei"}, frFR = {"Draeneï","Draeneï"}, itIT = {"Draenei","Draenei"}, koKR = {"드레나이","드레나이"}, ptBR = {"Draenei","Draenaia"}, ruRU = {"Дреней","Дренейка"}, zhCN = {"德莱尼","德莱尼"}, zhTW = {"德萊尼","德萊尼"} },
+	["worgen"] = { deDE = {"Worgen","Worgen"}, enUS = {"Worgen","Worgen"}, esES = {"Huargen","Huargen"}, frFR = {"Worgen","Worgen"}, itIT = {"Worgen","Worgen"}, koKR = {"늑대인간","늑대인간"}, ptBR = {"Worgen","Worgenin"}, ruRU = {"Ворген","Ворген"}, zhCN = {"狼人","狼人"}, zhTW = {"狼人","狼人"} },
+	["pandaren"] = { deDE = {"Pandaren","Pandaren"}, enUS = {"Pandaren","Pandaren"}, esES = {"Pandaren","Pandaren"}, frFR = {"Pandaren","Pandarène"}, itIT = {"Pandaren","Pandaren"}, koKR = {"판다렌","판다렌"}, ptBR = {"Pandaren","Pandarena"}, ruRU = {"Пандарен","Пандарен"}, zhCN = {"熊猫人","熊猫人"}, zhTW = {"熊貓人","熊貓人"} },
+
+	-- new in 7.3.5
+	["nightborne"] = { deDE = {"Nachtgeborener","Nachtgeborene"}, enUS = {"Nightborne","Nightborne"}, esES = {"Nocheterna","Nocheterna"}, esMX = {"Natonocturno","Natonocturna"}, frFR = {"Sacrenuit","Sacrenuit"}, itIT = {"Nobile Oscuro","Nobile Oscura"}, koKR = {"나이트본","나이트본"}, ptBR = {"Filho da Noite","Filha da Noite"}, ptPT = {"Filho da Noite","Filha da Noite"}, ruRU = {"Ночнорожденный","Ночнорожденная"}, zhCN = {"夜之子","夜之子"}, zhTW = {"夜裔精靈","夜裔"} },
+	["highmountaintauren"] = { deDE = {"Hochbergtauren","Hochbergtauren"}, enUS = {"Highmountain Tauren","Highmountain Tauren"}, esES = {"Tauren Monte Alto","Tauren Monte Alto"}, esMX = {"Tauren de Altamontaña","Tauren de Altamontaña"}, frFR = {"Tauren de Haut-Roc","Taurène de Haut-Roc"}, itIT = {"Tauren di Alto Monte","Tauren di Alto Monte"}, koKR = {"높은산 타우렌","높은산 타우렌"}, ptBR = {"Tauren Altamontês","Taurena Altamontesa"}, ptPT = {"Tauren Altamontês","Taurena Altamontesa"}, ruRU = {"Таурен Крутогорья","Тауренка Крутогорья"}, zhCN = {"至高岭牛头人","至高岭牛头人"}, zhTW = {"高嶺牛頭人","高嶺牛頭人"} },
+	["voidelf"] = { deDE = {"Leerenelf","Leerenelfe"}, enUS = {"Void Elf","Void Elf"}, esES = {"Elfo del Vacío","Elfa del Vacío"}, esMX = {"Elfo del Vacío","Elfa del Vacío"}, frFR = {"Elfe du Vide","Elfe du Vide"}, itIT = {"Elfo del Vuoto","Elfa del Vuoto"}, koKR = {"공허 엘프","공허 엘프"}, ptBR = {"Elfo Caótico","Elfa Caótica"}, ptPT = {"Elfo Caótico","Elfa Caótica"}, ruRU = {"Эльф Бездны","Эльфийка Бездны"}, zhCN = {"虚空精灵","虚空精灵"}, zhTW = {"虛無精靈","虛無精靈"} },
+	["lightforgeddraenei"] = { deDE = {"Lichtgeschmiedeter Draenei","Lichtgeschmiedete Draenei"}, enUS = {"Lightforged Draenei","Lightforged Draenei"}, esES = {"Draenei forjado por la Luz","Draenei forjada por la Luz"}, esMX = {"Draenei templeluz","Draenei templeluz"}, frFR = {"Draeneï sancteforge","Draeneï sancteforge"}, itIT = {"Draenei Forgialuce","Draenei Forgialuce"}, koKR = {"빛벼림 드레나이","빛벼림 드레나이"}, ptBR = {"Draenei Forjado a Luz","Draenaia Forjada a Luz"}, ptPT = {"Draenei Forjado a Luz","Draenaia Forjada a Luz"}, ruRU = {"Озаренный дреней","Озаренная дренейка"}, zhCN = {"光铸德莱尼","光铸德莱尼"}, zhTW = {"光鑄德萊尼","光鑄德萊尼"} },
+
+	-- new in 8.0
+	["zandalaritroll"] = { deDE = {"Zandalaritroll","Zandalaritroll"}, enUS = {"Zandalari Troll","Zandalari Troll"}, esES = {"Trol Zandalari","Trol Zandalari"}, esMX = {"Trol zandalari","Trol zandalari"}, frFR = {"Troll zandalari","Troll zandalari"}, itIT = {"Troll Zandalari","Troll Zandalari"}, koKR = {"잔달라 트롤","잔달라"}, ptBR = {"Troll Zandalari","Troll Zandalari"}, ptPT = {"Troll Zandalari","Troll Zandalari"}, ruRU = {"Зандалар","зандалар"}, zhCN = {"赞达拉巨魔","赞达拉巨魔"}, zhTW = {"贊達拉食人妖","贊達拉食人妖"} },
+	["kultiran"] = { deDE = {"Kul Tiraner","Kul Tiranerin"}, enUS = {"Kul Tiran","Kul Tiran"}, esES = {"Ciudadano de Kul Tiras","Ciudadana de Kul Tiras"}, esMX = {"Kultirano","Kultirana"}, frFR = {"Kultirassien","Kultirassienne"}, itIT = {"Kul Tirano","Kul Tirana"}, koKR = {"쿨 티란","쿨 티란"}, ptBR = {"Kultireno","Kultirena"}, ptPT = {"Kultireno","Kultirena"}, ruRU = {"Култирасец","Култираска"}, zhCN = {"库尔提拉斯人","库尔提拉斯人"}, zhTW = {"庫爾提拉斯人","庫爾提拉斯人"} },
+	["darkirondwarf"] = { deDE = {"Dunkeleisenzwerg","Dunkeleisenzwergin"}, enUS = {"Dark Iron Dwarf","Dark Iron Dwarf"}, esES = {"Enano Hierro Negro","Enana Hierro Negro"}, esMX = {"Enano Hierro Negro","Enana Hierro Negro"}, frFR = {"Nain sombrefer","Naine sombrefer"}, itIT = {"Nano Ferroscuro","Nana Ferroscuro"}, koKR = {"검은무쇠 드워프","검은무쇠 드워프"}, ptBR = {"Anão Ferro Negro","Anã Ferro Negro"}, ptPT = {"Anão Ferro Negro","Anã Ferro Negro"}, ruRU = {"Дворф из клана Черного Железа","Дворфийка из клана Черного Железа"}, zhCN = {"黑铁矮人","黑铁矮人"}, zhTW = {"黑鐵矮人","黑鐵矮人"} },
+	["magharorc"] = { deDE = {"Mag'har","Mag'har"}, enUS = {"Mag'har Orc","Mag'har Orc"}, esES = {"Orco Mag'har","Orco Mag'har"}, esMX = {"Orco mag'har","Orco mag'har"}, frFR = {"Orc mag’har","Orque mag’har"}, itIT = {"Orco Mag'har","Orchessa Mag'har"}, koKR = {"마그하르 오크","마그하르 오크"}, ptBR = {"Orc Mag'har","Orc Mag'har"}, ptPT = {"Orc Mag'har","Orc Mag'har"}, ruRU = {"Маг'хар","Маг'харка"}, zhCN = {"玛格汉兽人","玛格汉兽人"}, zhTW = {"瑪格哈獸人","瑪格哈獸人"} }
 };
 
 local function GetLanguageCode(lang)
